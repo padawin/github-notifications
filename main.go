7f457d1359264aef0229279f1f1b11a5fc3c7620
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2022-02-01 16:29:31 +0100

    Fix error output

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	c := NewGithubClientWithToken(os.Getenv("GITHUB_NOTIFICATIONS_TOKEN"))
 	notifs, err := c.getNotifications(context.Background())
 	if err != nil {
-		fmt.Println(" %{F#FC4D44}☠%{F#-} ")
+		fmt.Println(" %{F#FC4D44}☠%{F-} ")
 	} else if len(notifs) == 0 {
 		fmt.Println(" %{F#4E9A06}%{F-} ")
 	} else {
