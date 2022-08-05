83c04ced0ecd9031f9fd096c3037fc06bed41d42
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2022-08-05 13:57:43 +0200

    Fix error printing

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	c := NewGithubClientWithToken(os.Getenv("GITHUB_NOTIFICATIONS_TOKEN"))
 	notifs, err := c.getNotifications(context.Background())
 	if err != nil {
-		fmt.Print("%{B#FC4D44} ☠ %{B#-}")
+		fmt.Print("%{B#FC4D44}%{F#2E3436} ☠ %{B-}")
 	} else if len(notifs) > 0 {
 		fmt.Printf("%%{B#FC4D44}%%{F#2E3436}  %d %%{F-}%%{B-}", len(notifs))
 	}
